1b381cb6d8dfa616a7246f12518b400c83188c42
Nathan Herrera <nathanherrera@gmail.com>
2017-10-03 00:37:14 -0700

    Adds completed AuthButton specs and code to pass

--- src/components/session/AuthButton.vue
+++ src/components/session/AuthButton.vue
@@ -1,8 +1,17 @@
 <template lang="html">
-
+  <a :href="`${baseURL}auth/${authProvider}?origin=${authOriginURL}&hashmode=${hashMode}`">
+    <div id="auth-button" :style="backgroundStyle">
+      <span class="icon" :style="iconStyle"></span>
+      <span class="button-text" :style="textStyle">
+        <slot></slot>
+      </span>
+    </div>
+  </a>
 </template>
 
 <script>
+import router from '../../router'
+
 export default {
   props: {
     bgColor: {
@@ -25,9 +34,67 @@ export default {
       type: String,
       required: true
     }
+  },
+  computed: {
+    backgroundStyle () {
+      return {
+        background: this.bgColor
+      }
+    },
+    textStyle () {
+      return {
+        color: this.textColor
+      }
+    },
+    iconStyle () {
+      return {
+        background: `no-repeat center/110% url(${this.providerIcon})`
+      }
+    },
+    authOriginURL () {
+      if (process.env.NODE_ENV === 'development') {
+        return 'http://localhost:8080/'
+      }
+    },
+    hashMode () {
+      return router.mode === 'hash'
+    }
   }
 }
 </script>
 
 <style lang="css">
+  #auth-button {
+    display: inline-block;
+    background: white;
+    color: #444;
+    width: 190px;
+    border-radius: 2px;
+    /*border: thin solid #888;*/
+    white-space: nowrap;
+  }
+  #auth-button:hover {
+    box-shadow: 0 2px 28px rgba(0,0,0,0.25), 0 2px 10px rgba(0,0,0,0.22);
+    cursor: pointer;
+  }
+  #auth-button:active {
+    box-shadow: inset 0 0 0 32px rgba(0,0,0,0.1);
+  }
+  span.label {
+    font-family: serif;
+    font-weight: normal;
+  }
+  span.icon {
+    display: inline-block;
+    vertical-align: middle;
+    width: 42px;
+    height: 42px;
+  }
+  span.button-text {
+    display: inline-block;
+    vertical-align: middle;
+    padding-left: 6px;
+    font-size: 15px;
+    font-weight: bold;
+  }
 </style>
--- test/unit/specs/session/AuthButton.spec.js
+++ test/unit/specs/session/AuthButton.spec.js
@@ -1,12 +1,20 @@
 import AuthButton from '@/session/AuthButton'
 import { mount } from 'vue-test-utils'
+import router from 'src/router'
 
 describe('AuthButton.vue', () => {
+  // component relies almost entirely on data passed in by props
+  const testPropsData = {
+    bgColor: '#ff0000',
+    textColor: '#00ff00',
+    authProvider: 'test-provider',
+    providerIcon: '../statics/fancy_provider_icon.png',
+    baseURL: 'https://myApiWithOauth.com/'
+  }
   describe('props', () => {
     it('has a props object', () => {
       expect(AuthButton.props).to.be.a('object')
     })
-
     describe('bgColor', () => {
       it('validates String type', () => {
         expect(AuthButton.props.bgColor).to.be.a('object')
@@ -16,11 +24,7 @@ describe('AuthButton.vue', () => {
         expect(AuthButton.props.bgColor.default).to.equal('#ffffff')
       })
       it('receives valid data', () => {
-        const wrapper = mount(AuthButton, {
-          propsData: {
-            bgColor: '#ff0000'
-          }
-        })
+        const wrapper = mount(AuthButton, { propsData: testPropsData })
         assert(wrapper.hasProp('bgColor', '#ff0000'))
       })
     })
@@ -33,12 +37,8 @@ describe('AuthButton.vue', () => {
         expect(AuthButton.props.textColor.default).to.equal('#000000')
       })
       it('receives valid data', () => {
-        const wrapper = mount(AuthButton, {
-          propsData: {
-            textColor: '#ff0000'
-          }
-        })
-        assert(wrapper.hasProp('textColor', '#ff0000'))
+        const wrapper = mount(AuthButton, { propsData: testPropsData })
+        assert(wrapper.hasProp('textColor', '#00ff00'))
       })
     })
     describe('authProvider', () => {
@@ -50,11 +50,7 @@ describe('AuthButton.vue', () => {
         assert(AuthButton.props.authProvider.required)
       })
       it('receives valid data', () => {
-        const wrapper = mount(AuthButton, {
-          propsData: {
-            authProvider: 'test-provider'
-          }
-        })
+        const wrapper = mount(AuthButton, { propsData: testPropsData })
         assert(wrapper.hasProp('authProvider', 'test-provider'))
       })
     })
@@ -67,11 +63,7 @@ describe('AuthButton.vue', () => {
         assert(AuthButton.props.authProvider.required)
       })
       it('receives valid data', () => {
-        const wrapper = mount(AuthButton, {
-          propsData: {
-            providerIcon: '../statics/fancy_provider_icon.png'
-          }
-        })
+        const wrapper = mount(AuthButton, { propsData: testPropsData })
         assert(wrapper.hasProp('providerIcon', '../statics/fancy_provider_icon.png'))
       })
     })
@@ -84,12 +76,91 @@ describe('AuthButton.vue', () => {
         assert(AuthButton.props.baseURL.required)
       })
       it('receives valid data', () => {
-        const wrapper = mount(AuthButton, {
-          propsData: {
-            baseURL: 'https://myApiWithOauth.com/'
-          }
-        })
+        const wrapper = mount(AuthButton, { propsData: testPropsData })
         assert(wrapper.hasProp('baseURL', 'https://myApiWithOauth.com/'))
+      })
+    })
+  })
+  describe('computed properties', () => {
+    let wrapper
+    beforeEach(() => {
+      wrapper = mount(AuthButton, { propsData: testPropsData })
+    })
+    describe('backgroundStyle', () => {
+      it('is a function that returns an object with a background key', () => {
+        expect(AuthButton.computed.backgroundStyle).to.be.a('function')
+        expect(wrapper.vm.backgroundStyle.background).to.equal('#ff0000')
+      })
+    })
+    describe('textStyle', () => {
+      it('is a function that returns an object with a color key', () => {
+        expect(AuthButton.computed.textStyle).to.be.a('function')
+        expect(wrapper.vm.textStyle.color).to.equal('#00ff00')
+      })
+    })
+    describe('iconStyle', () => {
+      it('is a function that returns an object with a background key', () => {
+        expect(AuthButton.computed.iconStyle).to.be.a('function')
+        expect(wrapper.vm.iconStyle.background).to.match(/fancy_provider_icon/)
+      })
+    })
+    describe('authOriginURL', () => {
+      it('returns the current url scheme/host (string)', () => {
+        const envStub =
+          sinon.stub(process.env, 'NODE_ENV')
+            .returns('development')
+        expect(wrapper.vm.authOriginURL).to.equal('http://localhost:8080/')
+        envStub.restore()
+      })
+    })
+    describe('hashMode', () => {
+      it('checks the router mode and returns a boolean', () => {
+        const routerStub = sinon.stub(router, 'mode').returns('hash')
+        expect(wrapper.vm.hashMode).to.equal(true)
+        routerStub.restore()
+      })
+    })
+  })
+  describe('rendered content', () => {
+    let wrapper, envStub, routerStub
+    beforeEach(() => {
+      envStub = sinon.stub(process.env, 'NODE_ENV').returns('development')
+      routerStub = sinon.stub(router, 'mode').returns('hash')
+      wrapper = mount(AuthButton, {
+        propsData: testPropsData,
+        slots: { default: '<span>sign in with test-provider</span>' }
+      })
+    })
+    afterEach(() => {
+      envStub.restore()
+      routerStub.restore()
+    })
+    describe('top-level anchor tag', () => {
+      it('binds href attribute to computed properties', () => {
+        const aTag = wrapper.find('a')
+        expect(aTag.element.getAttribute('href'))
+          .to.equal(`${testPropsData.baseURL}auth/${testPropsData.authProvider}?origin=${wrapper.vm.authOriginURL}&hashmode=${wrapper.vm.hashMode}`)
+      })
+    })
+    describe('auth-button div', () => {
+      it('binds style attr background color from computed properties', () => {
+        const btnDiv = wrapper.find('#auth-button')
+        assert(btnDiv.hasStyle('background-color', testPropsData.bgColor))
+      })
+    })
+    describe('button text', () => {
+      it('renders button text from default slot', () => {
+        expect(wrapper.text()).to.include('sign in with test-provider')
+      })
+      it('binds text-color style attr from computed properties', () => {
+        const textSpan = wrapper.find('span.button-text')
+        assert(textSpan.hasStyle('color', testPropsData.textColor))
+      })
+    })
+    describe('button image', () => {
+      it('renders the button image inline', () => {
+        const imgSpan = wrapper.find('span.icon')
+        assert(imgSpan.hasStyle('background-image', `url(${testPropsData.providerIcon})`))
       })
     })
   })
