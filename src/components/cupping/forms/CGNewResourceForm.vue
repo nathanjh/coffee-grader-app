d7bcb71f4ab4128702e09a321074f67e549b6c79
Nathan Herrera <nathanherrera@gmail.com>
2017-11-02 01:07:21 -0700

    Adds new resource component and specs (passing)

--- src/components/cupping/forms/CGNewResourceForm.vue
+++ src/components/cupping/forms/CGNewResourceForm.vue
@@ -1,11 +1,75 @@
 <template lang="html">
-
+  <q-card>
+    <q-card-title align="center">
+      Add a new {{ model.name }}
+    </q-card-title>
+    <q-card-main>
+      <q-field
+        v-for="(value, attr, index) in form"
+        :key="index"
+        :error="errorClassCheck(attr)"
+        :error-label="errorMessageCheck(attr)"
+        :data-field-type="attr"
+      >
+        <q-input
+          :float-label="attr"
+          v-model="form[attr]"
+          @blur="touchIfValidates(attr)"
+        />
+      </q-field>
+    </q-card-main>
+    <q-card-actions align="around">
+      <q-btn
+        flat
+        class="col-2"
+        data-button-type="create-resource"
+        @click.prevent="createResource"
+      >
+        Submit
+      </q-btn>
+      <q-btn
+        color="teal"
+        flat
+        class="col-2"
+        data-button-type="clear-form"
+        @click.prevent="clearForm"
+      >
+        Reset
+      </q-btn>
+    </q-card-actions>
+  </q-card>
 </template>
 
 <script>
 /* generic 'new' resource form
    - meant to handle coffees, roasters, etc. */
+import { required } from 'vuelidate/lib/validators'
+import { validationMessages } from '@/mixins/validationMessages'
+import { mapGetters } from 'vuex'
+import CoffeeGraderApi from 'src/api/coffeeGraderApi'
+import {
+  QCard,
+  QCardTitle,
+  QCardMain,
+  QCardActions,
+  QField,
+  QInput,
+  QBtn,
+  Toast
+} from 'quasar'
+
 export default {
+  name: 'CGNewResourceForm',
+  mixins: [validationMessages],
+  components: {
+    QCard,
+    QCardTitle,
+    QCardMain,
+    QCardActions,
+    QField,
+    QInput,
+    QBtn
+  },
   props: {
     model: {
       type: Object,
@@ -17,10 +81,99 @@ export default {
         Array.isArray(model['attributes']) &&
         model['attributes'].every(i => typeof i === 'string')
       }
+    },
+    validates: {
+      type: Object,
+      required: false,
+      validator: function (validates) {
+        // currently only 'required' Vuelidate validator supported
+        for (const val in validates) {
+          const keys = Object.keys(validates[val])
+          if (keys.length !== 1 || keys[0] !== 'required') {
+            return false
+          }
+        }
+        return true
+      }
+    }
+  },
+  data () {
+    return this.parseModelProp()
+  },
+  validations () {
+    return this.parseValidatesProp()
+  },
+  computed: {
+    ...mapGetters(['authHeaders'])
+  },
+  methods: {
+    errorClassCheck (attr) {
+      if (this.$v.form[attr]) {
+        return this.$v.form[attr].$error
+      }
+    },
+    errorMessageCheck (attr) {
+      if (this.$v.form[attr] &&
+          Object.keys(this.$v.form[attr]).includes('required')) {
+        return this.requiredMessage(this.$v.form[attr], attr)
+      }
+    },
+    touchIfValidates (attr) {
+      if (this.$v.form[attr]) {
+        this.$v.form[attr].$touch()
+      }
+    },
+    parseModelProp () {
+      const dataModel = {
+        form: {}
+      }
+      this.model.attributes
+        .forEach(attr => {
+          dataModel.form[attr] = ''
+        })
+      return dataModel
+    },
+    parseValidatesProp () {
+      const validations = {
+        form: {}
+      }
+      for (const attr in this.validates) {
+        const options = this.validates[attr]
+        if (options.required) {
+          validations.form[attr] = { required }
+        }
+      }
+      return validations
+    },
+    createResource () {
+      this.$v.form.$touch()
+      if (this.$v.form.$error) {
+        Toast.create('Please review fields and try again.')
+        return
+      }
+      CoffeeGraderApi.post(`${this.model.name}s.json`, this.form, {
+        headers: this.authHeaders
+      })
+        .then(response => {
+          this.$emit(`${this.model.name}Created`, response.data)
+        })
+        .catch(error => {
+          error.response.data
+            .forEach(e => Toast.create({
+              html: e,
+              icon: 'error_outline'
+            }))
+        })
+    },
+    clearForm () {
+      for (const field in this.form) this.form[field] = ''
+      this.$v.form.$reset()
     }
   }
 }
 </script>
 
-<style lang="css">
+<style scoped lang="stylus">
+  div.q-card
+    padding 15px
 </style>
--- test/unit/specs/cupping/forms/CGNewResourceForm.spec.js
+++ test/unit/specs/cupping/forms/CGNewResourceForm.spec.js
@@ -1,6 +1,67 @@
+import 'babel-polyfill'
 import CGNewResourceForm from '@/cupping/forms/CGNewResourceForm'
+import { mount } from 'vue-test-utils'
+import CoffeeGraderApi from 'src/api/coffeeGraderApi'
+import Vue from 'vue'
+import Vuex from 'vuex'
+import Sessions from 'src/store/modules/sessions'
+import { required } from 'vuelidate/lib/validators'
+import { Toast } from 'quasar'
+
+Vue.use(Vuex)
 
 describe('CGNewResourceForm.vue', () => {
+  describe('rendered content', () => {
+    const model = {
+      name: 'coffee',
+      attributes: [
+        'name',
+        'producer',
+        'variety'
+      ]
+    }
+    const validates = {
+      name: { required: true },
+      producer: { required: true }
+    }
+    const store = new Vuex.Store({
+      modules: { Sessions }
+    })
+    let wrapper, createResourceButton, clearFormButton
+    beforeEach(() => {
+      wrapper = mount(CGNewResourceForm, {
+        store,
+        propsData: {
+          model,
+          validates
+        }
+      })
+      createResourceButton =
+        wrapper.find('[data-button-type="create-resource"]')
+      clearFormButton =
+        wrapper.find('[data-button-type="clear-form"]')
+    })
+    it('renders a form submit button', () => {
+      assert(createResourceButton.is('button'))
+      expect(createResourceButton.text()).to.include('Submit')
+    })
+    it('form submit button calls createResource method on click event', () => {
+      const submitHandler = sinon.stub(wrapper.vm, 'createResource')
+      createResourceButton.trigger('click')
+      expect(submitHandler.calledOnce).to.be.true
+      submitHandler.restore()
+    })
+    it('renders a clear-form button', () => {
+      assert(clearFormButton.is('button'))
+      expect(clearFormButton.text()).to.include('Reset')
+    })
+    it('clear-form button calls clearForm method on click event', () => {
+      const clearFormHandler = sinon.spy(wrapper.vm, 'clearForm')
+      clearFormButton.trigger('click')
+      expect(clearFormHandler.calledOnce).to.be.true
+      clearFormHandler.restore()
+    })
+  })
   describe('props', () => {
     const props = CGNewResourceForm.props
     const validModelProp = {
@@ -10,6 +71,13 @@ describe('CGNewResourceForm.vue', () => {
     it("has a required Object type 'model prop'", () => {
       expect(props.model.type).to.equal(Object)
       expect(props.model.required).to.equal(true)
+
+      const wrapper = mount(CGNewResourceForm, {
+        propsData: {
+          model: validModelProp
+        }
+      })
+      expect(wrapper.hasProp('model', validModelProp)).to.be.true
     })
     it("validates 'model' object has keys 'name' and 'attributes'", () => {
       const validator = props.model.validator
@@ -32,6 +100,375 @@ describe('CGNewResourceForm.vue', () => {
         name: 'invalid',
         attributes: ['1', 2, null, 4]
       })).to.equal(false)
+    })
+    it("has an optional Object type 'validates' prop", () => {
+      expect(props.validates.type).to.equal(Object)
+      expect(props.validates.required).to.equal(false)
+      const validationProp = {
+        name: {
+          required: true
+        }
+      }
+      const wrapper = mount(CGNewResourceForm, {
+        propsData: {
+          model: validModelProp,
+          validates: validationProp
+        }
+      })
+      expect(wrapper.hasProp('validates', validationProp))
+        .to.be.true
+    })
+    it('validates second-tier keys', () => {
+      const validator = props.validates.validator
+      expect(validator).to.be.a('function')
+
+      const validValidatorsProp = {
+        name: { required: true },
+        origin: { required: true }
+      }
+      const invalidProp = {
+        name: { minLength: true },
+        origin: { someOtherValidator: true },
+        producer: { required: true }
+      }
+      expect(validator(validValidatorsProp)).to.equal(true)
+      expect(validator(invalidProp)).to.equal(false)
+    })
+  })
+  describe('data', () => {
+    let wrapper
+    beforeEach(() => {
+      wrapper = mount(CGNewResourceForm, {
+        propsData: {
+          model: {
+            name: 'roaster',
+            attributes: [
+              'name',
+              'location',
+              'website'
+            ]
+          }
+        }
+      })
+    })
+    it("has an Object type 'form' property", () => {
+      expect(wrapper.vm.$data.form).to.be.a('object')
+    })
+    it("'form' object has keys that map to model props attribute values", () => {
+      const attributes = wrapper.vm.$props.model.attributes
+      const form = wrapper.vm.$data.form
+
+      expect(attributes.every(attr => Object.keys(form).includes(attr)))
+        .to.equal(true)
+    })
+  })
+  describe('form validation (Vuelidate)', () => {
+    const model = {
+      name: 'coffee',
+      attributes: [
+        'name',
+        'producer',
+        'variety'
+      ]
+    }
+    const validates = {
+      name: { required: true },
+      producer: { required: true }
+    }
+    let wrapper
+    beforeEach(() => {
+      wrapper = mount(CGNewResourceForm, {
+        propsData: {
+          model,
+          validates
+        }
+      })
+    })
+    context("given a 'validates' prop", () => {
+      it('validates the correct attributes by requirement', () => {
+        const attrs = Object.keys(validates)
+        const form = wrapper.vm.$v.form
+
+        expect(attrs.every(attr => {
+          return Object.keys(form[attr]).includes('required')
+        }))
+          .to.be.true
+      })
+      it('sets the error class for invalid fields', async function () {
+        // all fields from validates prop (attrs) are required
+        const fields = Object.keys(validates)
+        wrapper.vm.$v.form.$touch()
+
+        await wrapper.vm.$nextTick()
+
+        fields.forEach(field => {
+          const inputField = wrapper.find(`[data-field-type="${field}"]`)
+          expect(inputField.hasClass('q-field-with-error'))
+            .to.be.true
+        })
+      })
+    })
+  })
+  describe('computed properties', () => {
+    describe('authHeaders', () => {
+      const store = new Vuex.Store({
+        modules: { Sessions }
+      })
+      it('maps the authHeaders getter from the vuex store', () => {
+        const wrapper = mount(CGNewResourceForm, {
+          store,
+          propsData: {
+            model: {
+              name: 'roaster',
+              attributes: [
+                'name',
+                'location',
+                'website'
+              ]
+            }
+          }
+        })
+        // check for initial values match
+        expect(wrapper.vm.authHeaders)
+          .to.equal(store.getters.authHeaders)
+        // check after mutation committed to the store
+        store.commit('updateAuth', {
+          'access-token': 'xxxxx',
+          'expiry': 'yyyyy'
+        })
+        expect(wrapper.vm.authHeaders)
+          .to.deep.equal(store.getters.authHeaders)
+      })
+    })
+  })
+  describe('methods', () => {
+    const store = new Vuex.Store({
+      modules: { Sessions }
+    })
+    const model = {
+      name: 'coffee',
+      attributes: [
+        'name',
+        'producer',
+        'variety'
+      ]
+    }
+    const validates = {
+      name: { required: true },
+      producer: { required: true }
+    }
+    let wrapper
+    beforeEach(() => {
+      wrapper = mount(CGNewResourceForm, {
+        store,
+        propsData: {
+          model,
+          validates
+        }
+      })
+    })
+    describe('errorClassCheck', () => {
+      it('is a function', () => {
+        expect(wrapper.vm.errorClassCheck).to.be.a('function')
+      })
+      context("given a String type attribute included in 'validates' prop object", () => {
+        it('returns a boolean value', () => {
+          const attrs = Object.keys(validates)
+          attrs.forEach(attr => {
+            expect(wrapper.vm.errorClassCheck(attr))
+              .to.be.a('boolean')
+          })
+        })
+      })
+      context("given a String type attribute not included in 'validates' prop object", () => {
+        it('returns undefined', () => {
+          expect(wrapper.vm.errorClassCheck('variety'))
+            .to.be.undefined
+        })
+      })
+    })
+    describe('errorMessageCheck', () => {
+      it('is a function', () => {
+        expect(wrapper.vm.errorMessageCheck).to.be.a('function')
+      })
+      context("given a String type attribute included in 'validates' prop object", () => {
+        it('returns the correct error message', () => {
+          // 'name' is included in the 'validates' prop
+          expect(wrapper.vm.errorMessageCheck('name'))
+            .to.equal('name is required')
+        })
+      })
+      context("given a String type attribute not included in 'validates' prop object", () => {
+        it('returns undefined', () => {
+          expect(wrapper.vm.errorMessageCheck('variety'))
+            .to.be.undefined
+        })
+      })
+    })
+    describe('touchIfValidates', () => {
+      it('is a function', () => {
+        expect(wrapper.vm.touchIfValidates).to.be.a('function')
+      })
+      context("given a String type attribute included in 'validates'", () => {
+        it("sets the '$dirty' flag on the correct form field", () => {
+          // 'producer' is included in the 'validates' prop
+          const producerField = wrapper.vm.$v.form.producer
+          wrapper.vm.touchIfValidates('producer')
+          expect(producerField.$dirty).to.be.true
+        })
+      })
+      context("given a String type attribute not included in 'validates' prop object", () => {
+        it('returns undefined', () => {
+          expect(wrapper.vm.touchIfValidates('variety'))
+            .to.be.undefined
+        })
+      })
+    })
+    describe('parseModelProp', () => {
+      describe('maps the model prop to a new object', () => {
+        it("maps the model's attribute values to form object's keys", () => {
+          const parsedModel = wrapper.vm.parseModelProp()
+
+          expect(model.attributes.every(attr => {
+            return Object.keys(parsedModel.form).includes(attr)
+          })).to.equal(true)
+        })
+      })
+    })
+    describe('parseValidatesProp', () => {
+      describe('maps the validates prop to a new object', () => {
+        it("maps the validates top-level keys to form object's keys", () => {
+          expect(wrapper.vm.parseValidatesProp).to.be.a('function')
+          const form =
+            wrapper.vm.parseValidatesProp()
+              .form
+          expect(Object.keys(form))
+            .to.deep.equal(Object.keys(validates))
+        })
+        it('assigns the correct value each of form keys', () => {
+          const form =
+            wrapper.vm.parseValidatesProp()
+              .form
+          expect(Object.keys(form).every(attr => {
+            return form[attr].required === required
+          }))
+            .to.be.true
+        })
+      })
+    })
+    describe('createResource', () => {
+      it('is a function', () => {
+        expect(CGNewResourceForm.methods.createResource).to.be.a('function')
+      })
+      let apiCall
+      const coffeeData = {
+        coffee: {
+          id: 1,
+          name: 'some coffee',
+          producer: 'some producer',
+          variety: 'some variety'
+        }
+      }
+      beforeEach(() => {
+        apiCall = sinon.stub(CoffeeGraderApi, 'post')
+          .returns(Promise.resolve({
+            data: coffeeData
+          }))
+      })
+      afterEach(() => {
+        apiCall.restore()
+      })
+      describe('validates form input for errors', () => {
+        it('sets all form fields to dirty', () => {
+          wrapper.vm.createResource()
+          expect(wrapper.vm.$v.form.$dirty).to.be.true
+        })
+        it('creates a toast to communicate form input errors', () => {
+          const toastSpy = sinon.spy(Toast, 'create')
+          wrapper.vm.createResource()
+
+          expect(toastSpy.calledOnce).to.be.true
+          toastSpy.restore()
+        })
+      })
+      context('when form input is valid', () => {
+        beforeEach(() => {
+          wrapper.setData({
+            form: {
+              name: 'koooooona',
+              producer: 'farmer alfalfa'
+            }
+          })
+        })
+        it('calls the api with the correct url', async function () {
+          await wrapper.vm.createResource()
+          expect(apiCall.calledWith('coffees.json')).to.be.true
+        })
+        it('calls the api post method with the correct object', async function () {
+          await wrapper.vm.createResource()
+          const secondArg = apiCall.args[0][1]
+
+          expect(secondArg).to.deep.equal(wrapper.vm.form)
+        })
+        it('calls the api with correct auth headers', async function () {
+          store.commit('updateAuth', {
+            'access-token': 'xxxxx',
+            'expiry': 'yyyyy'
+          })
+          const correctAuthHeaders = {
+            headers: wrapper.vm.authHeaders
+          }
+
+          await wrapper.vm.createResource()
+          const thirdArg = apiCall.args[0][2]
+
+          expect(thirdArg).to.deep.equal(correctAuthHeaders)
+        })
+        context('on successful post response', () => {
+          it("emits a 'created' event with the correct name", async function () {
+            const spy = sinon.spy()
+            /* we're creating a 'coffee' resource,
+            so we'll expect a 'coffeeCreated' event' */
+            wrapper.vm.$on('coffeeCreated', spy)
+
+            await wrapper.vm.createResource()
+            expect(spy.called).to.be.true
+          })
+          it("emits a 'created' event with the correct payload", async function () {
+            const spy = sinon.spy()
+            wrapper.vm.$on('coffeeCreated', spy)
+
+            await wrapper.vm.createResource()
+            expect(spy.calledWith(coffeeData)).to.be.true
+          })
+        })
+      })
+    })
+    describe('clearForm', () => {
+      it('is a function', () => {
+        expect(wrapper.vm.clearForm).to.be.a('function')
+      })
+      beforeEach(() => {
+        wrapper.setData({
+          form: {
+            name: 'the dude',
+            producer: 'the valley',
+            variety: 'nice marmot'
+          }
+        })
+        // trigger form validations...
+        wrapper.vm.$v.form.$touch()
+      })
+      it('clears the form fields', () => {
+        wrapper.vm.clearForm()
+        for (const field in wrapper.vm.form) {
+          expect(wrapper.vm.form[field]).to.equal('')
+        }
+      })
+      it('resets the validation object $dirty flag', () => {
+        wrapper.vm.clearForm()
+        expect(wrapper.vm.$v.form.$dirty).to.equal(false)
+      })
     })
   })
 })
